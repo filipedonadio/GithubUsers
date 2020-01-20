34324d6edf05839c6defdf6556f2e808b0f80d3b
Filipe Donadio <filipe.donadio@gmail.com>
2020-01-20 08:54:38 +0100

    Initial commit

---
+++ GithubUsers/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  GithubUsers
+//
+//  Created by Filipe Donadio on 20/01/20.
+//  Copyright © 2020 Filipe Donadio. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray
+    }
+}
